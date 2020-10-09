f62aca3860a26d3d6bda0192535d7d16532e8eb1
Mohammed Aleem <aleem.btech@gmail.com>
2020-10-09 14:32:16 +0530

    oauth.ts changes

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,7 +1,7 @@
 export const OAuthSettings = {
   appId: 'e6c6ca55-7db5-46c0-b9bc-6ab0f71aadbf',
   authority: 'https://login.microsoftonline.com/68bdfeab-b118-489e-b3d3-9038010d3ef5',
-  redirectUri: 'http://localhost:4200',
+  redirectUri: 'https://msteams-demo-app.azurewebsites.net',//'http://localhost:4200',
   scopes: [
     "user.read",
     "calendars.read",
